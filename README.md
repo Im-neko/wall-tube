2823e477864eac6ae665d0878f86361d8774f833
im-neko <yuki@ideta.net>
2019-11-27 23:54:59 +0900

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# tube wall
+Play your Youtube Playlist as a WallPaper
+
+## Download
+Please download from this link
+https://im-neko.dev/walltube.zip
+
+
---
+++ main.js
@@ -0,0 +1,41 @@
+// TODO: 読み込み方バラバラでキモいのでそのうち直す
+import './auto-update';
+const electron = require('electron');
+const { ipcMain } = require('electron');
+
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+
+  let mainWindow = new BrowserWindow({
+    frame: false,
+    transparent: true,
+    resizable: true,
+    titleBarStyle: 'hidden',
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  mainWindow.loadURL('file://' + __dirname + '/index.html')
+  // mainWindow.openDevTools(); 
+  mainWindow.on('closed', function() {
+    mainWindow = null
+  });
+
+  ipcMain.on('loadStatus', (event, arg) => {
+    mainWindow.setIgnoreMouseEvents(arg);
+    mainWindow.maximize();
+  });
+
+});
+
