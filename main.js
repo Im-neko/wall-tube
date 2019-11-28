a2e691d42dd1de7c5c2e84e39df03e3456be394c
im-neko <yuki@ideta.net>
2019-11-28 16:20:54 +0900

    安定性の向上

--- main.js
+++ main.js
@@ -27,7 +27,6 @@ app.on('ready', function() {
   })
 
   mainWindow.loadURL('file://' + __dirname + '/index.html')
-  // mainWindow.openDevTools(); 
   mainWindow.on('closed', function() {
     mainWindow = null
     app.quit();
